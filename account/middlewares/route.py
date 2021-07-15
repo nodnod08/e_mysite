e8071e34a78e08a81f5ab5e6d78941a2b7f30e4a
Dondomie Dungca <dondomiedungca14@gmail.com>
2021-07-15 22:25:51 +0800

    latest 7/15/21

--- account/middlewares/route.py
+++ account/middlewares/route.py
@@ -11,6 +11,9 @@ class BaseMiddleware(object):
 class CustomAuthMiddleware(BaseMiddleware):
     def process_view(self, request, view_func, view_args, view_kwargs):
         if request.resolver_match.url_name == 'admin-index':
+            print("*****************")
+            print(request.resolver_match.kwargs)
+            print("*****************")
             if not request.user.is_authenticated:
                 return redirect('/admin/login')
         return None
